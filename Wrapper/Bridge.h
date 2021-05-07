c534dc2d51771e6709eb8514c285c7231756f394
Calpico <calpicodrink@protonmail.com>
2021-05-07 09:44:52 -0700

    forgot to add ROBLOX_LUA_TUSERDATA

--- Wrapper/Bridge.h
+++ Wrapper/Bridge.h
@@ -10,14 +10,14 @@
 namespace Bridge
 {
 	int registry;
-	DWORD m_rL;
+	DWORD m_RobloxState;
 	lua_State* m_L;
 	std::vector<int> int3breakpoints;
 
 	void Wrap(lua_State* State, DWORD RobloxState, int Index);
 	void UnWrap(DWORD RobloxState, lua_State* State, int Index);
 
-	int robloxBridge(DWORD rL);
+	int robloxBridge(DWORD RobloxState);
 	int vanillaBridge(lua_State* L);
 	int resumea(DWORD thread);
 }
@@ -76,6 +76,24 @@ namespace Bridge
 			}
 			roblox_lua_pop(RobloxState, 1);
 			break;
+		case ROBLOX_LUA_TUSERDATA:
+			roblox_lua_pushvalue(RobloxState, Index);
+			roblox_lua_pushstring(RobloxState, std::to_string(++registry).c_str());
+
+			roblox_lua_pushvalue(RobloxState, -2);
+			roblox_lua_settable(RobloxState, LUA_REGISTRYINDEX);
+			roblox_lua_pop(RobloxState, 1);
+			lua_newuserdata(State, 0);
+			lua_pushvalue(State, -1);
+			lua_pushstring(State, std::to_string(registry).c_str());
+			lua_settable(State, LUA_REGISTRYINDEX);
+			roblox_lua_getmetatable(RobloxState, Index);
+
+			UnWrap(RobloxState, State, -1);
+			roblox_lua_pop(RobloxState, 1);
+			lua_setmetatable(State, -2);
+			break;
+		deafult:break;
 		}
 	}
 
@@ -104,28 +122,28 @@ namespace Bridge
 
 
 
-	int robloxBridge(DWORD rL)
+	int robloxBridge(DWORD RobloxState)
 	{
 
 		lua_pushstring(m_L, std::to_string(++registry).c_str());
 		lua_State* L = lua_newthread(m_L);
 		lua_settable(m_L, LUA_REGISTRYINDEX);
 
-		int key = roblox_lua_tonumber(rL, lua_upvalueindex(1));
+		int key = roblox_lua_tonumber(RobloxState, lua_upvalueindex(1));
 
 		lua_rawgeti(L, LUA_REGISTRYINDEX, key);
 
-		for (int arg = 1; arg <= roblox_lua_gettop(rL); ++arg)
-			UnWrap(rL, L, arg);
+		for (int arg = 1; arg <= roblox_lua_gettop(RobloxState); ++arg)
+			UnWrap(RobloxState, L, arg);
 
-		switch (lua_pcall(L, roblox_lua_gettop(rL), LUA_MULTRET, 0))
+		switch (lua_pcall(L, roblox_lua_gettop(RobloxState), LUA_MULTRET, 0))
 		{
 
 
 		case LUA_YIELD:
 
-			roblox_lua_pushlightuserdata(m_rL, (void*)L);
-			roblox_lua_pushcclosure(m_rL, int3breakpoints[1], 1);
+			roblox_lua_pushlightuserdata(m_RobloxState, (void*)L);
+			roblox_lua_pushcclosure(m_RobloxState, int3breakpoints[1], 1);
 			return -1;
 		case LUA_ERRRUN:
 			printf("RVX ROBLOX ERROR: %s\n", lua_tostring(L, -1));
@@ -136,7 +154,7 @@ namespace Bridge
 		int args = 0;
 
 		for (int arg = 1; arg <= lua_gettop(L); ++arg, ++args)
-			Wrap(L, rL, arg);
+			Wrap(L, RobloxState, arg);
 
 		lua_settop(L, 0);
 
@@ -146,31 +164,31 @@ namespace Bridge
 
 	int vanillaBridge(lua_State* State) {
 
-		roblox_lua_pushstring(m_rL, std::to_string(++registry).c_str());
-		DWORD rL = roblox_lua_newthread(m_rL);
-		roblox_lua_settable(m_rL, LUA_REGISTRYINDEX);
+		roblox_lua_pushstring(m_RobloxState, std::to_string(++registry).c_str());
+		DWORD RobloxState = roblox_lua_newthread(m_RobloxState);
+		roblox_lua_settable(m_RobloxState, LUA_REGISTRYINDEX);
 
 		int key = lua_tonumber(State, lua_upvalueindex(1));
 
-		roblox_lua_rawgeti(rL, LUA_REGISTRYINDEX, key);
+		roblox_lua_rawgeti(RobloxState, LUA_REGISTRYINDEX, key);
 
 		for (int arg = 1; arg <= lua_gettop(State); ++arg)
-			Wrap(State, rL, arg);
+			Wrap(State, RobloxState, arg);
 
-		if (roblox_lua_pcall(rL, lua_gettop(State), LUA_MULTRET, 0))
+		if (roblox_lua_pcall(RobloxState, lua_gettop(State), LUA_MULTRET, 0))
 		{
-			const char* errormessage = roblox_lua_tostring(rL, -1, 0);
+			const char* errormessage = roblox_lua_tostring(RobloxState, -1, 0);
 
-			if (!errormessage || strlen(errormessage) == 0)
+			if (!errormessage || stRobloxStateen(errormessage) == 0)
 				errormessage = "Error occoured, no output from Lua\n";
 
 			if (strcmp(errormessage, "attempt to yield across metamethod/C-call boundary") == 0)
 			{
 
-				roblox_lua_pop(rL, 1);
+				roblox_lua_pop(RobloxState, 1);
 				lua_pushthread(State);
 				lua_pushcclosure(State, &resume, 1);
-				Wrap(State, rL, -1);
+				Wrap(State, RobloxState, -1);
 
 				return lua_yield(State, 0);
 			}
@@ -179,12 +197,12 @@ namespace Bridge
 
 		int args = 0;
 
-		for (int arg = 1; arg <= roblox_lua_gettop(rL); ++arg, ++args)
+		for (int arg = 1; arg <= roblox_lua_gettop(RobloxState); ++arg, ++args)
 		{
-			UnWrap(rL, State, arg);
+			UnWrap(RobloxState, State, arg);
 		}
 
-		roblox_lua_settop(rL, 0);
+		roblox_lua_settop(RobloxState, 0);
 
 		return args;
 		lua_close(State);
