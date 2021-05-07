2c95af57d12f15a225d04ec7893eede30c95e2ff
Calpico <calpicodrink@protonmail.com>
2021-05-07 16:32:05 -0700

    Pushing Globals

--- Wrapper/Main.cpp
+++ Wrapper/Main.cpp
@@ -38,6 +38,50 @@ void Init()
     lua_setfield(LuaState, -2, "__index");
 
 	// Pushing Globals
+
+    PushGlobal(RobloxState, LuaState, "game");
+    PushGlobal(RobloxState, LuaState, "Game");
+    PushGlobal(RobloxState, LuaState, "workspace");
+    PushGlobal(RobloxState, LuaState, "Workspace");
+    PushGlobal(RobloxState, LuaState, "Axes");
+    PushGlobal(RobloxState, LuaState, "BrickColor");
+    PushGlobal(RobloxState, LuaState, "CFrame");
+    PushGlobal(RobloxState, LuaState, "Color3");
+    PushGlobal(RobloxState, LuaState, "ColorSequence");
+    PushGlobal(RobloxState, LuaState, "ColorSequenceKeypoint");
+    PushGlobal(RobloxState, LuaState, "NumberRange");
+    PushGlobal(RobloxState, LuaState, "NumberSequence");
+    PushGlobal(RobloxState, LuaState, "NumberSequenceKeypoint");
+    PushGlobal(RobloxState, LuaState, "PhysicalProperties");
+    PushGlobal(RobloxState, LuaState, "Ray");
+    PushGlobal(RobloxState, LuaState, "Rect");
+    PushGlobal(RobloxState, LuaState, "Region3");
+    PushGlobal(RobloxState, LuaState, "Region3int16");
+    PushGlobal(RobloxState, LuaState, "TweenInfo");
+    PushGlobal(RobloxState, LuaState, "UDim");
+    PushGlobal(RobloxState, LuaState, "UDim2");
+    PushGlobal(RobloxState, LuaState, "Vector2");
+    PushGlobal(RobloxState, LuaState, "Vector2int16");
+    PushGlobal(RobloxState, LuaState, "Vector3");
+    PushGlobal(RobloxState, LuaState, "Vector3int16");
+    PushGlobal(RobloxState, LuaState, "Enum");
+    PushGlobal(RobloxState, LuaState, "Faces");
+    PushGlobal(RobloxState, LuaState, "Instance");
+    PushGlobal(RobloxState, LuaState, "math");
+    PushGlobal(RobloxState, LuaState, "warn");
+    PushGlobal(RobloxState, LuaState, "typeof");
+    PushGlobal(RobloxState, LuaState, "type");
+    PushGlobal(RobloxState, LuaState, "spawn");
+    PushGlobal(RobloxState, LuaState, "Spawn");
+    PushGlobal(RobloxState, LuaState, "print");
+    PushGlobal(RobloxState, LuaState, "printidentity");
+    PushGlobal(RobloxState, LuaState, "ypcall");
+    PushGlobal(RobloxState, LuaState, "Wait");
+    PushGlobal(RobloxState, LuaState, "wait");
+    PushGlobal(RobloxState, LuaState, "delay");
+    PushGlobal(RobloxState, LuaState, "Delay");
+    PushGlobal(RobloxState, LuaState, "tick");
+    PushGlobal(RobloxState, LuaState, "LoadLibrary");
 }
 
 BOOL APIENTRY DllMain( HMODULE hModule,
