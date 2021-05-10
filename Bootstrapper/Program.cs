7aa28175926f544eb03561899ba06e4d635bb302
Calpico <calpicodrink@protonmail.com>
2021-05-09 21:34:42 -0700

    create simple bootstrapper

--- Bootstrapper/Program.cs
+++ Bootstrapper/Program.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
 
 namespace Bootstrapper
 {
     class Program
     {
+        static string Host = "";
+        static string TempZipURL = Host + "/files/temp.zip";
+        static string BinFolder = Directory.GetCurrentDirectory() + "\\bin";
+        static string ZipFileDest = BinFolder + "\\Temp.zip";
+        static string Executable = BinFolder + "\\Revival.exe";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (!Directory.Exists(BinFolder))
+            {
+                Directory.CreateDirectory(BinFolder);
+
+                using (WebClient Client = new WebClient())
+                {
+                    Client.DownloadFile(TempZipURL, ZipFileDest);
+                    ZipFile.ExtractToDirectory(ZipFileDest, BinFolder);
+                }
+
+                Process.Start(Executable);
+            }
         }
     }
 }
